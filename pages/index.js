6b047522b23112726be6d7f03fcd185d012b0a2b
DIR\adam.kloczl <adam.kloczl@accenture.com>
2022-10-24 11:03:34 +0200

    changed some text to test CI/CD

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      This is the initial project setup. Go to <a href='/api/trades'>Trades</a> to see some data.
+    </div>
+  );
 };
 
 export default HomePage;
