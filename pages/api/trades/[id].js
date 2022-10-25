66c8f69f72dc187d079672f6644e786059406235
DIR\adam.kloczl <adam.kloczl@accenture.com>
2022-10-25 14:44:31 +0200

    added dynamic api route

--- pages/api/trades/[id].js
+++ pages/api/trades/[id].js
@@ -2,11 +2,12 @@ import { collection, getDocs } from 'firebase/firestore/lite';
 import { getDB } from '@api';
 
 const tradeHandler = async (req, res) => {
+  const { id } = req.query;
   const db = getDB();
   if (!db) {
     res.status(500).json('Not Authorized');
   }
-  const trades = collection(db, 'trades/1m_30d_WS20_LS5_10_20/trades');
+  const trades = collection(db, `trades/${id}/trades`);
   const tradesSnapshot = await getDocs(trades);
   const tradesList = tradesSnapshot.docs.map((doc) => doc.data());
   res.status(200).json(tradesList);
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Link from 'next/link';
+import Header from '@components/header';
+import axios from 'axios';
 
 const HomePage = () => {
+  useEffect(() => {
+    const getData = async () => {
+      const data = await axios.get('/api/trades');
+      console.log(data);
+    };
+    getData();
+  }, []);
+
   return (
-    <div>
-      This is the initial project setup. Go to <Link href='/api/trades'>Trades</Link> to see some data.
-    </div>
+    <>
+      <Header />
+      <div>
+        This is the initial project setup. Go to <Link href='/api/trades'>Trades</Link> to see some data.
+      </div>
+    </>
   );
 };
 
