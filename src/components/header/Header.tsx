5a4a3b0850d877c894929c80496ed834675aa830
DIR\adam.kloczl <adam.kloczl@accenture.com>
2022-11-07 11:41:13 +0100

    added colored dots, and detailed tooltip

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,10 +4,15 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header className={styles.header}>
+    <header className={styles.container}>
       <h1 className={styles.h1}>Crypto predict</h1>
       <nav className={styles.nav}>
-        <Link href={'/api/trades'}>Bots data</Link>
+        <div className={styles.nav__item}>
+          <Link href={'/'}>Home</Link>
+        </div>
+        <div className={styles.nav__item}>
+          <Link href={'/api/trades'}>Bots data</Link>
+        </div>
       </nav>
     </header>
   );
